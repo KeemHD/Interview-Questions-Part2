1e60b3d1eaef744987fd759c3d1de8772998b025
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-08-26 08:29:53 -0400

    Adding Find Subtree Question

--- Interview_Questions_Continued2.py
+++ Interview_Questions_Continued2.py
@@ -197,3 +197,50 @@ print("<-----------------START--------------<")
 print(tree_level_max_sum(n1))
 # Should print 1 as level 1 has the highest level sum
 print("<-----------------END--------------<")
+
+#8/25/20
+#Hi, here's your problem today.
+# This problem was recently asked by Apple:
+
+#Given 2 binary trees t and s, find if s
+# has an equal subtree in t, where the structure
+# and the values are the same. Return True if it exists,
+# otherwise return False.
+
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return f"(Value: {self.value} Left: {self.left} Right: {self.right})"
+
+def find_subtree(s, t):
+    # Fill this in.
+    print()
+
+t3 = Node(4, Node(3), Node(2))
+t2 = Node(5, Node(4), Node(-1))
+t = Node(1, t2, t3)
+
+s = Node(4, Node(3), Node(2))
+"""
+Tree t:
+    1
+   / \
+  4   5 
+ / \ / \
+3  2 4 -1
+
+Tree s:
+  4 
+ / \
+3  2 
+"""
+
+print("Find Subtree Sum 8-25")
+print("<-----------------START--------------<")
+print(find_subtree(s, t))
+# True
+print("<-----------------END--------------<")
