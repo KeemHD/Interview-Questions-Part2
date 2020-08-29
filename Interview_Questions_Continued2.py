20ed47d0a0fbb6377d2c09916653301d75766798
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-08-29 12:14:42 -0400

    Adding String to Integer Question

--- Interview_Questions_Continued2.py
+++ Interview_Questions_Continued2.py
@@ -302,3 +302,28 @@ print("<-----------------START--------------<")
 print(f"0b{swap_bits(0b10101010101010101010101010101010):032b}")
 # 0b01010101010101010101010101010101
 print("<-----------------END--------------<")
+
+#8/29/20
+#Hi, here's your problem today.
+# This problem was recently asked by LinkedIn:
+
+#Given a string, convert it to an integer
+# without using the builtin str function.
+# You are allowed to use ord to convert a
+# character to ASCII code.
+
+#Consider all possible cases of an integer.
+# In the case where the string is not a valid integer,
+# return None.
+
+
+def convert_to_int(s):
+    # Fill this in.
+    print(s)
+    return 0
+
+print("String to Integer 8-29")
+print("<-----------------START--------------<")
+print(convert_to_int('-105') + 1)
+# -104
+print("<-----------------END--------------<")
