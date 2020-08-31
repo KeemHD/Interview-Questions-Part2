12a01c777688a674e6bfa7ce15b27721777ca6e2
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-08-30 20:18:37 -0400

    Adding Rotate Matrix Question

--- Interview_Questions_Continued2.py
+++ Interview_Questions_Continued2.py
@@ -327,3 +327,31 @@ print("<-----------------START--------------<")
 print(convert_to_int('-105') + 1)
 # -104
 print("<-----------------END--------------<")
+
+#8/30/20
+#Hi, here's your problem today.
+# This problem was recently asked by Uber:
+
+#Given a square 2D matrix (n x n),
+# rotate the matrix by 90 degrees clockwise.
+
+
+def rotate(mat):
+    # Fill this in.
+    print(mat)
+
+mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# Looks like
+# 1 2 3
+# 4 5 6
+# 7 8 9
+
+# Looks like
+# 7 4 1
+# 8 5 2
+# 9 6 3
+print("Rotate Matrix 8-30")
+print("<-----------------START--------------<")
+print(rotate(mat))
+# [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+print("<-----------------END--------------<")
