cc8f59262515a3a106192eb990e98371abd83d76
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-09-08 15:26:17 -0400

    Adding Queens on a chessboard Question

--- Interview_Questions_Continued2.py
+++ Interview_Questions_Continued2.py
@@ -544,3 +544,32 @@ remove_dup(lst)
 print(lst)
 # (1, (2, (3, None)))
 print("<-----------------END--------------<")
+
+
+#9/7/20
+#Hi, here's your problem today.
+# This problem was recently asked by Microsoft:
+
+#N-Queens is the problem where you
+# find a way to put n queens on a nxn
+# chess board without them being able
+# to attack each other. Given an integer
+# n, return 1 possible solution by returning
+# all the queen's position.
+
+def n_queens(n):
+    # Fill this in.
+    print(n)
+
+print("Queens on a chessboard 9-7")
+print("<-----------------START--------------<")
+print(n_queens(5))
+# There can be many answers
+# [(0, 0), (1, 2), (2, 4), (3, 1), (4, 3)]
+
+# Q . . . .
+# . . . Q .
+# . Q . . .
+# . . . . Q
+# . . Q . .
+print("<-----------------END--------------<")
