542fd42d00933fed3a3e6740ac55ac1cdaec9166
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-09-09 17:12:22 -0400

    Adding Number of Connected Componets Question

--- Interview_Questions_Continued2.py
+++ Interview_Questions_Continued2.py
@@ -611,3 +611,24 @@ print(remove_outermost_parenthesis('(()())'))
 print(remove_outermost_parenthesis('()()()'))
 # ' '
 print("<-----------------END--------------<")
+
+#9/9/20
+#Hi, here's your problem today.
+# This problem was recently asked by Apple:
+
+#Given a list of undirected edges which represents a graph,
+# find out the number of connected components.
+
+#In the above example, vertices 1, 2, 3, 4 are all connected,
+# and 5, 6 are connected, and thus there are 2 connected components
+# in the graph above.
+
+def num_connected_components(edges):
+    # Fill this in.
+    print(edges)
+
+print("Number of Connected Componets 9-9")
+print("<-----------------START--------------<")
+print(num_connected_components([(1, 2), (2, 3), (4, 1), (5, 6)]))
+# 2
+print("<-----------------END--------------<")
