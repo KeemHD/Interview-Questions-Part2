a1ba030be4332f41a0aec11520a1676910a4c36b
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-09-13 15:49:53 -0400

    Adding Duplicate Subtrees Question

--- Interview_Questions_Continued2.py
+++ Interview_Questions_Continued2.py
@@ -752,3 +752,58 @@ print("<-----------------START--------------<")
 print(closest_points([(0, 0), (1, 2), (-3, 4), (3, 1)], 2))
 # [(1, 2), (0, 0)]
 print("<-----------------END--------------<")
+
+#9/13/20
+#Hi, here's your problem today.
+# This problem was recently asked by Uber:
+
+#Given a binary tree, find all duplicate
+# subtrees (subtrees with the same value and same structure)
+# and return them as a list of list [subtree1, subtree2, ...]
+# where subtree1 is a duplicate of subtree2 etc.
+
+
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        if self.left and self.right:
+            return f"({self.value}, {self.left}, {self.right})"
+        if self.left:
+            return f"({self.value}, {self.left})"
+        if self.right:
+            return f"({self.value}, None, {self.right})"
+        return f"({self.value})"
+
+def dup_trees(root):
+    # Fill this in.
+    print()
+
+n3_1 = Node(3)
+n2_1 = Node(2, n3_1)
+n3_2 = Node(3)
+n2_2 = Node(2, n3_2)
+n1 = Node(1, n2_1, n2_2)
+# Looks like
+#     1
+#    / \
+#   2   2
+#  /   /
+# 3   3
+
+print("Duplicate Subtrees 9-13")
+print("<-----------------START--------------<")
+print(dup_trees(n1))
+# [[(3), (3)], [(2, (3)), (2, (3))]]
+# There are two duplicate trees
+#
+#     2
+#    /
+#   3
+# and just the leaf
+#
+# 3
+print("<-----------------END--------------<")
