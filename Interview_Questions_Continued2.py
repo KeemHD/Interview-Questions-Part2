cdf4db5baff6b71e904d6be2f832e0772fdd1c70
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-09-14 16:05:56 -0400

    Adding K Closest Elements Question

--- Interview_Questions_Continued2.py
+++ Interview_Questions_Continued2.py
@@ -807,3 +807,21 @@ print(dup_trees(n1))
 #
 # 3
 print("<-----------------END--------------<")
+
+#9/14/20
+#Hi, here's your problem today.
+# This problem was recently asked by AirBNB:
+
+#Given a list of sorted numbers,
+# and two integers k and x, find k
+# closest numbers to the pivot x.
+
+def closest_nums(nums, k, x):
+    # Fill this in.
+    print(nums,k,x)
+
+print("K Closest Elements 9-14")
+print("<-----------------START--------------<")
+print(closest_nums([1, 3, 7, 8, 9], 3, 5))
+# [7, 3, 8]
+print("<-----------------END--------------<")
