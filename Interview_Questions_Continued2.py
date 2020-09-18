7404623dbc051faffd89fc7d41533783d1919f8d
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-09-17 23:48:58 -0400

    Adding Index of Larger Next Number Question

--- Interview_Questions_Continued2.py
+++ Interview_Questions_Continued2.py
@@ -819,9 +819,53 @@ print("<-----------------END--------------<")
 def closest_nums(nums, k, x):
     # Fill this in.
     print(nums,k,x)
+    '''found = False
+    start_pos = 0
+    end_pos = len(nums)-1
+    mid_pos = int((start_pos + end_pos)/2)
+
+    while start_pos <= end_pos:
+
+        if nums[mid_pos] > k:
+            end_pos = mid_pos - 1
+
+        elif nums[mid_pos] < k:
+            start_pos = mid_pos + 1
+
+        else:
+            found = True
+            break
+
+        mid_pos = int((start_pos + end_pos)/2)
+
+    if found:
+        print(nums[mid_pos])
+    else:
+        print(str(k) + " was not found in the list: " + str(nums))'''
 
 print("K Closest Elements 9-14")
 print("<-----------------START--------------<")
 print(closest_nums([1, 3, 7, 8, 9], 3, 5))
 # [7, 3, 8]
+print("<-----------------END--------------<")
+
+#9/15/20
+#Hi, here's your problem today.
+# This problem was recently asked by Google:
+
+#Given a list of numbers, for each element
+# find the next element that is larger than the current element.
+# Return the answer as a list of indices.
+# If there are no elements larger than the current
+# element, then use -1 instead.
+
+def larger_number(nums):
+    # Fill this in.
+    print(nums)
+
+
+print("Index of Larger Next Number 9-15")
+print("<-----------------START--------------<")
+print(larger_number([3, 2, 5, 6, 9, 8]))
+# print [2, 2, 3, 4, -1, -1]
 print("<-----------------END--------------<")
