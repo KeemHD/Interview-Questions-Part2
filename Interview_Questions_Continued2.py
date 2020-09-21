dfc39872ce6fa820ba75042c333a1752b8f23140
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-09-21 12:28:50 -0400

    Adding Multiply Question

--- Interview_Questions_Continued2.py
+++ Interview_Questions_Continued2.py
@@ -1143,3 +1143,25 @@ print("<-----------------START--------------<")
 print(to_hex(123))
 # 7B
 print("<-----------------END--------------<")
+
+#9/21/20
+#Hi, here's your problem today.
+# This problem was recently asked by Microsoft:
+
+#Given two strings which represent non-negative
+# integers, multiply the two numbers and return
+# the product as a string as well. You should
+# assume that the numbers may be sufficiently
+# large such that the built-in integer type will
+# not be able to store the input (Python does have
+# BigNum, but assume it does not exist).
+
+def multiply(str1, str2):
+    # Fill this in.
+    print(str1,str2)
+
+print("Multiply 9-21")
+print("<-----------------START--------------<")
+print(multiply("11", "13"))
+# 143
+print("<-----------------END--------------<")
