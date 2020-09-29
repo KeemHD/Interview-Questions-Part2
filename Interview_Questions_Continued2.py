2046caf2bbb72f55c0ff16715439749680573fe1
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-09-29 10:32:09 -0400

    Adding Intersection of Lists Question

--- Interview_Questions_Continued2.py
+++ Interview_Questions_Continued2.py
@@ -1284,3 +1284,21 @@ print(transpose(mat))
 #  [2,5],
 #  [3,6]]
 print("<-----------------END--------------<")
+
+#9/25/20
+#Hi, here's your problem today.
+# This problem was recently asked by Twitter:
+
+#Given 3 sorted lists, find the intersection of those 3 lists.
+
+
+def intersection(list1, list2, list3):
+    # Fill this in.
+    print()
+
+print("Intersection of Lists 9-25")
+print("<-----------------START--------------<")
+print(intersection([1, 2, 3, 4], [2, 4, 6, 8], [3, 4, 5]))
+# [4]
+print("<-----------------END--------------<")
+
