a9619171403c560886a3cd8cc12c965b2464ddf3
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-10-01 21:19:23 -0400

    Adding Reshaping Matrix Question

--- Interview_Questions_Continued2.py
+++ Interview_Questions_Continued2.py
@@ -1461,3 +1461,28 @@ print("<-----------------START--------------<")
 print(sum_binary("11101", "1011"))
 # 101000
 print("<-----------------END--------------<")
+
+#10/1/20
+#Hi, here's your problem today.
+# This problem was recently asked by Facebook:
+
+#Reshaping a matrix means to take the same elements
+# in a matrix but change the row and column length.
+# This means that the new matrix needs to have the
+# same elements filled in the same row order as the
+# old matrix. Given a matrix, a new row size x and
+# a new column size y, reshape the matrix.
+# If it is not possible to reshape, return None.
+
+def reshape_matrix(mat, x, y):
+    # Fill this in.
+    print()
+
+print("Reshaping Matrix 10-1")
+print("<-----------------START--------------<")
+print(reshape_matrix([[1, 2], [3, 4]], 1, 4))
+# [[1], [2], [3], [4]]
+
+print(reshape_matrix([[1, 2], [3, 4]], 2, 3))
+# None
+print("<-----------------END--------------<")
