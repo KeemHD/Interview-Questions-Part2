566453237872b8ecc850a860d23d369fa3f9ce89
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-10-06 08:32:55 -0400

    Adding 4 Sum Question

--- Interview_Questions_Continued2.py
+++ Interview_Questions_Continued2.py
@@ -1602,7 +1602,7 @@ print(integer_to_roman(444))
 print("<-----------------END--------------<")
 
 
-#10/6/20
+#10/4/20
 #Hi, here's your problem today.
 # This problem was recently asked by Microsoft:
 
@@ -1646,4 +1646,31 @@ print(longest_common_prefix(['helloworld', 'hellokitty', 'hell']))
 
 print(longest_common_prefix(['daily', 'interview', 'pro']))
 # ''
+print("<-----------------END--------------<")
+
+
+
+#10/5/20
+#Hi, here's your problem today.
+# This problem was recently asked by Microsoft:
+
+#Given a list of numbers, and a target number n,
+# find all unique combinations of a, b, c, d,
+# such that a + b + c + d = n.
+
+
+def fourSum(nums, target):
+    # Fill this in.
+    print(nums, target)
+
+print("4 Sum 10-5")
+print("<-----------------START--------------<")
+print(fourSum([1, 1, -1, 0, -2, 1, -1], 0))
+# print [[-1, -1, 1, 1], [-2, 0, 1, 1]]
+
+print(fourSum([3, 0, 1, -5, 4, 0, -1], 1))
+# print [[-5, -1, 3, 4]]
+
+print(fourSum([0, 0, 0, 0, 0], 0))
+# print ([0, 0, 0, 0])
 print("<-----------------END--------------<")
