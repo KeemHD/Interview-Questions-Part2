25f3596fd978c9c7d5fed5367f81a59d41f1ac85
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-10-16 01:48:29 -0400

    Adding Target Sum from Root to Leaf Question

--- Interview_Questions_Continued2.py
+++ Interview_Questions_Continued2.py
@@ -1822,3 +1822,39 @@ print("<-----------------START--------------<")
 print(min_depth_bst(n1))
 # 2
 print("<-----------------END--------------<")
+
+#10/11/20
+#Hi, here's your problem today.
+# This problem was recently asked by Uber:
+
+#Given a binary tree, and a target number,
+# find if there is a path from the root to
+# any leaf that sums up to the target.
+
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+def target_sum_bst(root, target):
+    # Fill this in.
+    print()
+
+#      1
+#    /   \
+#   2     3
+#    \     \
+#     6     4
+n6 = Node(6)
+n4 = Node(4)
+n3 = Node(3, None, n4)
+n2 = Node(2, None, n6)
+n1 = Node(1, n2, n3)
+
+print("Target Sum from Root to Leaf 10-11")
+print("<-----------------START--------------<")
+print(target_sum_bst(n1, 9))
+# True
+# Path from 1 -> 2 -> 6
+print("<-----------------END--------------<")
