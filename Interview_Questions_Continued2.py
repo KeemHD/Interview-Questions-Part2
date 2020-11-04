dbcd550070f15a161736ebfb1c1a81241fae2321
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-11-04 10:50:58 -0500

    Adding Three Equal Sums Question

--- Interview_Questions_Continued2.py
+++ Interview_Questions_Continued2.py
@@ -2725,3 +2725,26 @@ print(n)
 print("<-----------------END--------------<")
 
 
+#11/4/20
+#Hi, here's your problem today.
+# This problem was recently asked by Apple:
+
+#Given an array of numbers,
+# determine whether it can be partitioned into 3 arrays of equal sums.
+
+#For instance,
+#[0, 2, 1, -6, 6, -7, 9, 1, 2, 0, 1] can be partitioned into:
+#[0, 2, 1], [-6, 6, -7, 9, 1], [2, 0, 1] all of which sum to 3.
+
+class Solution(object):
+    def canThreePartsEqualSum(self, A):
+        # Fill this in.0
+        print(A)
+
+print("Three Equal Sums 11-4")
+print("<-----------------START--------------<")
+print(Solution().canThreePartsEqualSum(
+    [0, 2, 1, -6, 6, -7, 9, 1, 2, 0, 1]))
+# True
+print("<-----------------END--------------<")
+
