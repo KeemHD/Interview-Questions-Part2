e20cf18fd6dced7f9fc1fc2b10843ab54ef9161d
Hakeem Dixon <36652282+KeemHD@users.noreply.github.com>
2020-11-05 10:57:23 -0500

    Adding Remove duplicates from Linked List Question

--- Interview_Questions_Continued2.py
+++ Interview_Questions_Continued2.py
@@ -2748,3 +2748,37 @@ print(Solution().canThreePartsEqualSum(
 # True
 print("<-----------------END--------------<")
 
+
+#11/5/20
+#Hi, here's your problem today.
+# This problem was recently asked by Twitter:
+
+#Given a linked list,
+# remove all duplicate values from the linked list.
+
+#For instance, given 1 -> 2 -> 3 -> 3 -> 4,
+# then we wish to return the linked list 1 -> 2 -> 4.
+
+class Node(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+    def __str__(self):
+        if not self.next:
+            return str(self.val)
+        return str(self.val) + " " + str(self.next)
+
+class Solution(object):
+    def deleteDuplicates(self, node):
+        # Fill this in.
+        print()
+
+n = Node(1, Node(2, Node(3, Node(3, Node(4)))))
+print("Remove duplicates from Linked List 11-5")
+print("<-----------------START--------------<")
+print(n)
+# 1 2 3 3 4
+Solution().deleteDuplicates(n)
+print(n)
+# 1 2 4
+print("<-----------------END--------------<")
